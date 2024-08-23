0c4e21be015c933350a4826c9d830ea73a41e117
Vishnumaya <vm6553088@gmail.com>
2024-08-23 10:06:29 +0530

    q1

---
+++ q1.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+  const f = createHelloWorld();
+console.log(f());
+ // "Hello W orld"
+ 
